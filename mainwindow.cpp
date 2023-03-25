7a1a0e3e8017715897184e8031b1754f0a350945
TheStoneGoblin <21307377@studentmail.ul.ie>
2023-03-25 20:25:50 +0000

    changed buttons to checkboxes, added radioButton for size of pizza

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,10 @@
 #include <QVBoxLayout>
 #include <QPushButton>
 #include <QLabel>
+#include <QRadioButton>
+#include <QCheckBox>
+#include <QButtonGroup>
+
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -13,12 +17,22 @@ MainWindow::MainWindow(QWidget *parent)
 
     QWidget *widget = new QWidget;
 
-    QPushButton *button1 = new QPushButton("A");
-    QPushButton *button2 = new QPushButton("B");
-    QPushButton *button3 = new QPushButton("C");
-    QPushButton *button4 = new QPushButton("D");
-    QPushButton *button5 = new QPushButton("E");
-    QPushButton *button6 = new QPushButton("F");
+    QCheckBox *button1 = new QCheckBox("Mushroom");
+    QCheckBox *button2 = new QCheckBox("Corn");
+    QCheckBox *button3 = new QCheckBox("Ham");
+    QCheckBox *button4 = new QCheckBox("Olive");
+    QCheckBox *button5 = new QCheckBox("Ice Cream");
+    QPushButton *button6 = new QPushButton("Make Pizza");
+
+    QButtonGroup *group = new QButtonGroup;
+    QRadioButton *radio1 = new QRadioButton("7 inch");
+    QRadioButton *radio2 = new QRadioButton("9 inch");
+    QRadioButton *radio3 = new QRadioButton("12 inch");
+    QRadioButton *radio4 = new QRadioButton("16 inch");
+    group->addButton(radio1);
+    group->addButton(radio2);
+    group->addButton(radio3);
+    group->addButton(radio4);
 
     QLabel *label1 = new QLabel("A");
     QLabel *label2 = new QLabel("B");
@@ -27,17 +41,24 @@ MainWindow::MainWindow(QWidget *parent)
     QLabel *label5 = new QLabel("E");
 
     QGridLayout *layout = new QGridLayout;
-    layout->addWidget(button1, 0, 0);
-    layout->addWidget(button2, 1, 0);
-    layout->addWidget(button3, 2, 0);
-    layout->addWidget(button4, 3, 0);
-    layout->addWidget(button5, 4, 0);
-    layout->addWidget(label1, 0, 1);
-    layout->addWidget(label2, 1, 1);
-    layout->addWidget(label3, 2, 1);
-    layout->addWidget(label4, 3, 1);
-    layout->addWidget(label5, 4, 1);
-    layout->addWidget(button6, 5, 0, 1, 2);
+    layout->addWidget(button1, 0, 0, 1, 2);
+    layout->addWidget(button2, 1, 0, 1, 2);
+    layout->addWidget(button3, 2, 0, 1, 2);
+    layout->addWidget(button4, 3, 0, 1, 2);
+    layout->addWidget(button5, 4, 0, 1, 2);
+
+    layout->addWidget(label1, 0, 3, 1, 2);
+    layout->addWidget(label2, 1, 3, 1, 2);
+    layout->addWidget(label3, 2, 3, 1, 2);
+    layout->addWidget(label4, 3, 3, 1, 2);
+    layout->addWidget(label5, 4, 3, 1, 2);
+
+    layout->addWidget(radio1, 5, 0);
+    layout->addWidget(radio2, 5, 1);
+    layout->addWidget(radio3, 5, 2);
+    layout->addWidget(radio4, 5, 3);
+
+    layout->addWidget(button6, 6, 0, 1, 4);
 
     widget->setLayout(layout);
     widget->show();
